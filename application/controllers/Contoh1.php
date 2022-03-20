a6a68fe53355cc9cb9748a9d70cbe738a7fb72ac
AsepHadianto <asephad@gmail.com>
2022-03-20 20:46:30 +0700

    First Try

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+  public function index()
+  {
+    echo "<h1>Perkenalkan</h1>";
+    echo "Nama saya Asep Hadianto
+    Saya tinggal di daerah Tanjung Priok
+    olahraga yang saya sukai adalah Bulutangkis dan renang";
+  }
+}
