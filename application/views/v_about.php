9888bbebd5a54a775d8f10df859e3cbb3e846050
AsepHadianto <asephad@gmail.com>
2022-04-05 16:38:18 +0700

    Commit Kedua

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+  <h1><?php echo $judul ?></h1>
+  <h4>Nama</h4>
+  <ul type="disc">
+    <li>Nama Depan : Asep</li>
+    <li>Nama Belakang : Hadianto</li>
+  </ul>
+  <br>
+  <h4>Alamat</h4>
+  <ul type="none">
+    <li>Jalan Enim No. 17D Tanjung Priok</li>
+  </ul>
+
+  <h4>Tempat Lahir</h4>
+  <ul type="none">
+    <li>Kuningan</li>
+  </ul>
+
+  <h4>Olahraga Favorit</h4>
+  <ul type="square">
+    <li>Bulutangkis</li>
+  </ul>
+</section>
